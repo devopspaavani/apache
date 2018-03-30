598590f85b4f8bc25e011006d091772e8c7d278f
pavani <devopspaavani@gmail.com>
2018-03-29 23:33:42 -0500

    updated file resources

--- recipes/server.rb
+++ recipes/server.rb
@@ -10,6 +10,21 @@ template '/var/www/html/index.html' do
  source 'index.html.erb'
 end
 
+cookbook_file '/var/www/html/sample.html' do
+  source 'sample.txt'
+end
+
+remote_file '/var/www/html/metadata.html' do
+  source 'https://github.com/devopspaavani/apache/blob/master/metadata.rb'
+end
+
+template '/var/www/html/sample.txt' do
+ source 'sample.txt.erb'
+end
+
+
+
+
 service 'apache2' do
   action [:enable, :start]
 end
